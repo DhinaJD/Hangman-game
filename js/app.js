0fa8daa3568cd956b804f9255a01e4bca1bc5d74
DhinaJD <dhinakaran.jayaraj17@gmail.com>
2023-01-16 18:54:15 +0530

    states completed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 import Home from "./modules/home.js";
+import Game from "./modules/game.js";
 
 Home.init();
+Game.init();
--- js/modules/game.js
+++ js/modules/game.js
@@ -1,7 +1,26 @@
 const Game = (_ =>{
 
+  //states
+  const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const words  = ["apple", "money", "cat", "dog", "power", "flower", "ninja", "game"];
+  let chosenWord;
+  let guessingWord;
+  let lives;
+  let guesses;
+
+  //catch the dom
+  const $hangman = document.querySelector(".hangman");
+
   const init = _=>{
-    console.log("game enter");
+    chosenWord =  choseWord();
+    guessingWord = Array(chosenWord).fill("_")
+    guesses = [];
+    lives = 7;
+  }
+
+  const choseWord = _ =>{
+    let randomNum = Math.floor(Math.random() * words.length);
+    return randomNum;
   }
 
 
